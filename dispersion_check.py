4b753c641fad6b71d8b7324eb306b06312f2c65c
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2023-12-18 15:52:33 +0700

    Update dispersion_check.py

--- dispersion_check.py
+++ dispersion_check.py
@@ -11,11 +11,12 @@ if picture is not None:
     # To read image file buffer with OpenCV:
     bytes_data = picture.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_GRAYSCALE)
-    st.image(cv2_img)
-    st.write(cv2_img)
+    # st.image(cv2_img)
+    # st.write(cv2_img)
 
 
-    _, edged = cv2.threshold(cv2_img, 20, 255, cv2.THRESH_BINARY)
+    _, edged = cv2.threshold(cv2_img, 240, 255, cv2.THRESH_BINARY)
+    # st.image(edged)
     # cv2.imshow("(Edged)", edged)
     # cv2.waitKey(0)
     cnts, _ = cv2.findContours(
@@ -24,16 +25,16 @@ if picture is not None:
         cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE,
     )
-    print(f"Cnts Simple Shape (1): {cnts[0].shape}")
-    print(f"Cnts Simple Shape (2): {cnts[0].shape}")
-    cnts2, _ = cv2.findContours(
-        # does this need to be changed?
-        edged,
-        cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_NONE,
-    )
-    print(f"Cnts NoApprox Shape:{cnts2[0].shape}")
-    print(cnts)
+    # print(f"Cnts Simple Shape (1): {cnts[0].shape}")
+    # print(f"Cnts Simple Shape (2): {cnts[0].shape}")
+    # cnts2, _ = cv2.findContours(
+    #     # does this need to be changed?
+    #     edged,
+    #     cv2.RETR_EXTERNAL,
+    #     cv2.CHAIN_APPROX_NONE,
+    # )
+    # print(f"Cnts NoApprox Shape:{cnts2[0].shape}")
+    # print(cnts)
     canvas = np.ones(cv2_img.shape)
     cv2.drawContours(canvas, cnts, -1, (0, 255, 255), 1)
     st.image(canvas)
@@ -41,4 +42,7 @@ if picture is not None:
     # cv2.waitKey(0)
     st.write(f"Found {len(cnts)} shapes!")
 
-    st.image(picture)
+    # st.image(picture)
+
+
+
